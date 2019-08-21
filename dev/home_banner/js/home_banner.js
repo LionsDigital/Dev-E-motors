de567d2b555156f4b29b69fd034dd065d2903a89
root <root@vps.server.local>
2019-08-21 18:35:15 +0000

    21-08-2019 15h - Banner da home Mobile

---
+++ dev/home_banner/js/home_banner.js
@@ -0,0 +1,15 @@
+let i = 0;
+let txtUm = 'Melhor opção';
+let txtDois = 'em mobilidade';
+let txtBt = 'Conheça a marca';
+let speed = 10;
+
+document.querySelector(".home_banner").addEventListener("load", typeWriter());
+
+function typeWriter() {
+      document.querySelector(".home_banner--texto-um").innerHTML += txtUm.charAt(i);
+      document.querySelector(".home_banner--texto-dois").innerHTML += txtDois.charAt(i);
+      document.querySelector(".home_banner--botao a").innerHTML += txtBt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+}
